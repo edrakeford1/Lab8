7fbe5de406e52e63b73db8032761d6c063a926fe
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-03-06 15:24:13 -0500

    checkpoint

--- lab8_edrakeford_1.py
+++ lab8_edrakeford_1.py
@@ -28,3 +28,14 @@ def find_UPC(first_11_digits):
         check_digit = 10 - modulo
     
     return check_digit
+
+# Main program
+upc = input("Enter a 12-digit UPC: ")
+
+first_11_digits = upc[:11]
+last_digit = upc[11]
+
+print("\nThe first 11 digits are '" +first_11_digits+ "'")
+print("The provided check digit is '" +last_digit+ "'")
+print("\nCaluculating...")
+
