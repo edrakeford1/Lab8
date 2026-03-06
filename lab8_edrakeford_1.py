039f0b27c5f8c66eac2bba22dd07197d9dc399e3
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-03-06 16:16:18 -0500

    find_UPC working

--- lab8_edrakeford_1.py
+++ lab8_edrakeford_1.py
@@ -12,13 +12,13 @@ def find_UPC(first_11_digits):
     odd_sum = 0
     even_sum = 0
 
-    for i in range(1,12):
-        digit = int(first_11_digits[i])
+    for i in range(0, 11):
+        digit = first_11_digits[i]
 
         if i % 2 == 0:
-            odd_sum += digit
+            odd_sum += int(digit)
         else:
-            even_sum += digit
+            even_sum += int(digit)
 
     total = (odd_sum * 3) + even_sum
     modulo = total % 10
@@ -35,7 +35,9 @@ upc = input("Enter a 12-digit UPC: ")
 first_11_digits = upc[:11]
 last_digit = upc[11]
 
-print("\nThe first 11 digits are '" +first_11_digits+ "'")
-print("The provided check digit is '" +last_digit+ "'")
+print(f"\nThe first 11 digits are '" +first_11_digits+ "'")
+print(f"The provided check digit is '" +last_digit+ "'")
 print("\nCaluculating...")
 
+expected_digit = find_UPC(first_11_digits)
+print("The expected check digit is", expected_digit, ".")
